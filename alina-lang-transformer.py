c2cf9df1d327bc392d6586c807aa790924ba4ffe
Edick1337 <2n331.hello@gmail.com>
2024-03-11 11:38:44 +0300

    Add files via upload

---
+++ alina-lang-transformer.py
@@ -0,0 +1,36 @@
+import urllib.request
+import xml.etree.ElementTree as ET
+from datetime import datetime
+import os
+
+url = "https://translations.telegram.org/eliteng/android/export"
+formatters = ["formatterMonthYear", "formatterStats12H", "formatterStats24H", "formatterBannedUntil12H",
+              "formatterBannedUntil24H", "formatterBannedUntilThisYear12H", "formatterBannedUntilThisYear24H",
+              "formatterMonthYear2", "formatterMonthName", "formatterMonth", "formatterYear", "formatterYearMax",
+              "chatDate", "chatFullDate", "formatterWeek", "formatterWeekLong", "formatterDay24H", "formatterDay12H",
+              "formatDateAtTime", "formatDateSchedule", "formatDateScheduleYear", "SendTodayAt", "SendDayAt",
+              "SendDayYearAt", "StartTodayAt", "StartDayAt", "StartDayYearAt", "StartsTodayAt", "StartsDayAt",
+              "StartsDayYearAt", "StartShortTodayAt", "StartShortDayAt", "StartShortDayYearAt", "RemindTodayAt",
+              "RemindDayAt", "RemindDayYearAt"]
+
+response = urllib.request.urlopen(url)
+xml_data = response.read()
+
+root = ET.fromstring(xml_data)
+
+for elem in root.iter():
+    if elem.attrib.get("name") in formatters:
+        continue
+    if elem.text:
+        elem.text = ' '.join(elem.text.lower().strip(' "').split())
+
+nameTemplate = "edited_strings"
+
+for file in os.listdir():
+    if nameTemplate in file:
+        os.remove(file)
+
+now = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+filename = f"{nameTemplate}_{now}.xml"
+tree = ET.ElementTree(root)
+tree.write(filename)
