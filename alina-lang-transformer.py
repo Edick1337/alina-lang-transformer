7f69597c82bed6739c42ae3697bb6e453f77ba25
Edick1337 <2n331.hello@gmail.com>
2024-03-14 18:54:50 +0300

    Update alina-lang-transformer.py
    Beautified

--- alina-lang-transformer.py
+++ alina-lang-transformer.py
@@ -5,14 +5,25 @@ import os
 import pytz
 
 url = "https://translations.telegram.org/eliteng/android/export"
-formatters = ["formatDateSchedule", "formatDateScheduleDay", "formatterMonthYear", "formatterStats12H", "formatterStats24H", "formatterBannedUntil12H",
-              "formatterBannedUntil24H", "formatterBannedUntilThisYear12H", "formatterBannedUntilThisYear24H",
-              "formatterMonthYear2", "formatterMonthName", "formatterMonth", "formatterYear", "formatterYearMax",
-              "chatDate", "chatFullDate", "formatterWeek", "formatterWeekLong", "formatterDay24H", "formatterDay12H",
-              "formatDateAtTime", "formatDateSchedule", "formatDateScheduleYear", "SendTodayAt", "SendDayAt",
-              "SendDayYearAt", "StartTodayAt", "StartDayAt", "StartDayYearAt", "StartsTodayAt", "StartsDayAt",
-              "StartsDayYearAt", "StartShortTodayAt", "StartShortDayAt", "StartShortDayYearAt", "RemindTodayAt",
-              "RemindDayAt", "RemindDayYearAt"]
+formatters = [
+    "chatDate",
+    "chatFullDate",
+    "SendTodayAt",
+    "SendDayAt",
+    "SendDayYearAt",
+    "StartTodayAt",
+    "StartDayAt",
+    "StartDayYearAt",
+    "StartsTodayAt",
+    "StartsDayAt",
+    "StartsDayYearAt",
+    "StartShortTodayAt",
+    "StartShortDayAt",
+    "StartShortDayYearAt",
+    "RemindTodayAt",
+    "RemindDayAt",
+    "RemindDayYearAt",
+]
 
 response = urllib.request.urlopen(url)
 xml_data = response.read()
@@ -20,10 +31,13 @@ xml_data = response.read()
 root = ET.fromstring(xml_data)
 
 for elem in root.iter():
-    if elem.attrib.get("name") in formatters:
+    name_attr = elem.attrib.get("name")
+    if name_attr is None:
+        continue
+    if name_attr in formatters or name_attr.startswith("format"):
         continue
     if elem.text:
-        elem.text = ' '.join(elem.text.lower().strip(' "').split())
+        elem.text = " ".join(elem.text.lower().strip(' "').split())
 
 nameTemplate = "eliteng"
 
@@ -31,7 +45,7 @@ for file in os.listdir():
     if nameTemplate in file:
         os.remove(file)
 
-moscow_tz = pytz.timezone('Europe/Moscow')
+moscow_tz = pytz.timezone("Europe/Moscow")
 now = datetime.now(moscow_tz).strftime("%d-%m-%Y")
 filename = f"{nameTemplate}_{now}.xml"
 tree = ET.ElementTree(root)
