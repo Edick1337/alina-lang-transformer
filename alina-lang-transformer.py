74dfcf3d39c2a9ef4973b6e740f75223a5872093
Edick1337 <2n331.hello@gmail.com>
2024-08-07 16:40:54 +0300

    Update alina-lang-transformer.py

--- alina-lang-transformer.py
+++ alina-lang-transformer.py
@@ -53,5 +53,4 @@ tree.write(filename)
 
 env_file = os.getenv('GITHUB_ENV')
 with open(env_file, "w") as myfile:
-    myfile.write(f"LANGUAGE_NAME={language_name}\n")
-    myfile.write(f"FILE_NAME={filename}")
+    myfile.write(f"DATE={now}\nLANGUAGE_NAME={language_name}\nFILE_NAME={filename}")
